b10130b006776af360615d23deb0b379949595c0
DivoHub <39216431+DivoHub@users.noreply.github.com>
2021-11-09 23:47:34 -0800

    before bed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+PATH = (r"C:\Users\HeDar\Documents\GitHub\RoadTestBot\chromedriver.exe")
+
+driver = webdriver.Chrome(PATH)
+driver.get("https://www.icbc.com/driver-licensing/visit-dl-office/Pages/Book-a-road-test.aspx")
+
+book_button = driver.find_element_by_link_text("Book online")
+book_button.click()
 
 
+time.sleep(5)
